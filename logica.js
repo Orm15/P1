609273c05e0561f8ce49733951d09a6f08c7199d
Orm15 <20100835@ue.edu.pe>
2023-04-01 18:31:37 -0500

    primer comentario

---
+++ logica.js
@@ -0,0 +1,35 @@
+
+miComboBox.onchange = function() {
+    var valorC = document.getElementById("miComboBox").value;
+
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var estado = "";
+
+
+    if(isNaN(num1) || isNaN(num2)){ //isNaN devuelve true si la variable no es un numero
+        estado = "Operacion no valida ";
+        document.getElementById("resultado").value = estado;
+        
+    }else{
+        if(valorC == "1"){
+            var result = num1 + num2;
+        }else if(valorC == "2"){
+            var result = num1 - num2;
+        }else if(valorC == "3"){
+            var result = num1 * num2;
+        }else if(valorC == "4"){
+            var result = num1 / num2;
+        }else if (valorC == "5"){
+            document.getElementById("resultado").value = "Selecciona una operacion";
+            
+        }
+
+        document.getElementById("resultado").value = "";
+        document.getElementById("resultado").value = result;
+    }
+
+
+    
+
+};
