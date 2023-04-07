5a1490f7e173de47b8dcebd64681477965097986
Orm15 <20100835@ue.edu.pe>
2023-04-07 16:59:08 -0500

    Ultima modificacion

--- logica.js
+++ logica.js
@@ -4,26 +4,26 @@ miComboBox.onchange = function() {
 
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
-    var estado = "";
-
+    let resultado;
 
     if(isNaN(num1) || isNaN(num2)){ //isNaN devuelve true si la variable no es un numero
         alert('Escribe numeros en los espacios que lo soliciten')
-        
+        document.getElementById("miComboBox").value= "5";
+        document.getElementById("resultado").value = "";
     }else{
         if(valorC == "1"){
-            var result = num1 + num2;
+             result = num1 + num2;
         }else if(valorC == "2"){
-            var result = num1 - num2;
+             result = num1 - num2;
         }else if(valorC == "3"){
-            var result = num1 * num2;
+             result = num1 * num2;
         }else if(valorC == "4"){
-            var result = num1 / num2;
+             result = num1 / num2;
         }else if (valorC == "5"){
             document.getElementById("resultado").value = "Selecciona una operacion";
             
         }
-
+        document.getElementById("miComboBox").value= "5";
         document.getElementById("resultado").value = "";
         document.getElementById("resultado").value = result;
     }
